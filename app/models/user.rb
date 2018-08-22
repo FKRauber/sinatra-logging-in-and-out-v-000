e3b2168c6691b96b81847eadbcee31497e422eae
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-22 21:19:11 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,3 @@
-class User
-  attr_accessor :username, :password, :balance
-
-  def initialize(args)
-    @username = args(:username)
-    @password = args(:password)
-    @balance = args(:balance)
-  end
+class User < ActiveRecord::Base
 
 end
